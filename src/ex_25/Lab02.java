a42d7aa3807e0423c24446fef736c9deda833129
rajat solanki <rajatsolanki021@gmail.com>
2025-03-25 22:38:11 +0530

    all file added

---
+++ src/ex_25/Lab02.java
@@ -0,0 +1,16 @@
+package ex_25;
+
+public class Lab02 {
+public static void main(String args[]){
+
+    boolean b= true;
+    boolean c= false;
+    System.out.println(b);
+
+    int a =100;
+    System.out.println(a);
+    long bc = 300;
+    System.out.println(bc);
+    System.out.println(a+bc);
+}
+}
